dcfa312ed491df524c32babfb9b4e92b031635c8
Ferran Puig <ferran@playcreatividad.com>
2013-04-12 00:33:56 +0100

    forall was ok, it was map that was wrong :S that happens to be doing it tso late xD

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -56,7 +56,7 @@ object FunSets {
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
       if (  a > bound ) true
-      else if (contains(s, a) && !p(a)) false
+      else if (s(a) && !p(a)) false
       else iter(a+1)
     }
     iter(-bound)
@@ -71,7 +71,7 @@ object FunSets {
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = (x: Int) => exists(s , (y:Int) => f(x) == y )
+  def map(s: Set, f: Int => Int): Set = (x: Int) => exists ( s , (y:Int) => f(y) == x )
 
   /**
    * Displays the contents of a set
